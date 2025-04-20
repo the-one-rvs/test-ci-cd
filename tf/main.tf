cddd47f205e4be32f7d3255259c61d819626be19
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-04-20 14:55:23 +0530

    Done

--- tf/main.tf
+++ tf/main.tf
@@ -16,8 +16,8 @@ resource "aws_security_group" "vaibhav_strapi_sg" {
     }
 
     ingress {
-        from_port   = 1337
-        to_port     = 1337
+        from_port   = 80
+        to_port     = 80
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
